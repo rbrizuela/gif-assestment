1c61bb8e78a21ac7d2ddc50c1840333e06cc4fe3
Brizuela Roberto <rbrizuela@tresases.local>
2019-03-08 11:39:15 -0300

    Updates

---
+++ precache-manifest.25b500e352207f5d8217fdb3b6427ee6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b58662c982617a96cb28",
+    "url": "/gif-assestment/static/css/main.90514363.chunk.css"
+  },
+  {
+    "revision": "b58662c982617a96cb28",
+    "url": "/gif-assestment/static/js/main.b58662c9.chunk.js"
+  },
+  {
+    "revision": "636ff1898bdb146ecc79",
+    "url": "/gif-assestment/static/js/runtime~main.636ff189.js"
+  },
+  {
+    "revision": "dbdaf468ad995bb1f7bc",
+    "url": "/gif-assestment/static/js/2.dbdaf468.chunk.js"
+  },
+  {
+    "revision": "dae6358d82a0f2cb2621330a26ffb64c",
+    "url": "/gif-assestment/index.html"
+  }
+];
