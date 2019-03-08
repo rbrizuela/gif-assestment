b8727bcbcc53508b130eb7542dce56f4af2f59b1
Brizuela Roberto <rbrizuela@tresases.local>
2019-03-08 11:27:01 -0300

    commit

---
+++ build/precache-manifest.de875176f7da22138b27ea45bec2d898.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d599363385ff9a9e590",
+    "url": "/static/css/main.90514363.chunk.css"
+  },
+  {
+    "revision": "4d599363385ff9a9e590",
+    "url": "/static/js/main.4d599363.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dbdaf468ad995bb1f7bc",
+    "url": "/static/js/2.dbdaf468.chunk.js"
+  },
+  {
+    "revision": "bb25effd12b42122318ddc5ccb504e0e",
+    "url": "/index.html"
+  }
+];
